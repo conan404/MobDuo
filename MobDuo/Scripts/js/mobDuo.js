8f85dfa376f3741b8fc663cf15990bc8506025f4
Alex Kyprianou <conan404@gmail.com>
2012-10-11 13:03:27 +0100

    Added in ads

--- MobDuo/Scripts/js/mobDuo.js
+++ MobDuo/Scripts/js/mobDuo.js
@@ -1,1 +1,16 @@
-﻿
+﻿var MobDuo = {
+    topBannerId : 'topBanner',
+    bottomBannerId : 'bottomBanner',
+    containerId : 'adContainer',
+
+    adloc : '/Ads/flat/ExampleFlatAd.jpg',
+
+    initialiseAds : function (){
+        
+
+        $(this.topBannerId).append(this.adloc);    
+
+        $(this.bottomBannerId).append(this.adloc);    
+    }   
+}
+
